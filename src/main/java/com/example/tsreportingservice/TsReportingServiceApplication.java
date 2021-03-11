c89f1b929fb54946027963d4b9d2a2254466ec4d
penbosso <ckbosso@st.ug.edu.gh>
2021-03-11 15:35:02 +0000

    initial setup

---
+++ src/main/java/com/example/tsreportingservice/TsReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsreportingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsReportingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tsreportingservice/TsReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsreportingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
