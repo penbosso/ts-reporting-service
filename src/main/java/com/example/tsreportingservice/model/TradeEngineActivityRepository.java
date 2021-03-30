3aa0a0c3efa2ff361100f84aae10200c0f1a70a2
Jojo-Jowey <44313712+Jojo-Jowey@users.noreply.github.com>
2021-03-30 12:43:27 +0000

    No changes

---
+++ src/main/java/com/example/tsreportingservice/model/TradeEngineActivityRepository.java
@@ -0,0 +1,6 @@
+package com.example.tsreportingservice.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TradeEngineActivityRepository extends JpaRepository<TradeEngineActivity, Long> {
+}
